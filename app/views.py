8d9e05fc5fe24b44065a712baa0d9c5fd9a446e4
micha003 <michael.vorogushyn@gmail.com>
2025-11-21 21:35:28 +0100

    my first real commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+
+# NEVER, I REPEAT NEVERRRR - touch this code snippet pls <3
+#
+# just keep it for the sake of it
+def mibombo(mi: str, bombo: bool) -> str:
+    # TODO: make it work ig
+    return mi + bombo
