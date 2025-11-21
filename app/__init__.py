2f647f9c1ae225cd8404e021d2077c3f9f8d52f8
micha003 <michael.vorogushyn@gmail.com>
2025-11-21 22:54:00 +0100

    added api script (hw) and now it should be ready

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask
+from flask_restful import Api
 from dotenv import load_dotenv
 import os
 
 # import the blueprints for the routes
 from .views import views
 
+load_dotenv()
 
 def create_app():
     app = Flask("app")
+    api = Api(app)
+
     app.config["SECRET_KEY"] = os.getenv("MY_SECRET_KEY")
 
     app.register_blueprint(views, url_prefix="/")
+
+    from .api import HelloWorld
+    api.add_resource(HelloWorld, '/')
 
     return app
--- app/views.py
+++ app/views.py
@@ -13,7 +13,7 @@ import json
 
 views = Blueprint("views", __name__)
 
-@views.route("/")
+@views.route("/home")
 def home():
     return render_template("home.html")
 
